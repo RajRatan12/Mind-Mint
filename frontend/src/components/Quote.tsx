0f82953e74f2349b84073e068e9ec44020864c7b
RajRatan12 <rajratancse@gmail.com>
2024-06-11 11:07:26 +0530

    cors resolved and signup and signin page completed

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -5,13 +5,13 @@ export const Quote = () => {
         <div className="flex justify-center">
             <div className="max-w-lg">
                 <div className="text-3xl font-bold">
-                    "The customer support I received was exceptional. The support team went above and beyond to address my concerns"
+                “Unleash your voice, ignite minds. Every word you share crafts a world of its own.”
                 </div>
                 <div className="max-w-md text-xl font-semibold text-left mt-4">
-                    Julies Winfield
+                    Raj Ratan
                 </div>
                 <div className="max-w-md text-sm font-light text-slate-400">
-                    CEO | Acme corp
+                    CEO | MindMint
                 </div>
             </div>
         </div>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import { Quote } from '../components/Quote'
+import { Auth } from "../components/Auth"
+import { Quote } from "../components/Quote"
 
 export const Signup = () => {
-  return (
-    <div>
-        <div className="grid grid-cols-2">
-        <div>
-
+    return <div>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+            <div>
+                <Auth type="signup" />
+            </div>
+            <div className="hidden lg:block">
+                <Quote />
+            </div>
         </div>
-        <Quote/>
-        </div>
-        
     </div>
-    
-  )
 }
