7ed0b5f133bcd1ce5607f0b30eba4ca4b8cd0dd0
RajRatan12 <rajratancse@gmail.com>
2024-06-16 09:38:54 +0530

    debugging

--- frontend/src/components/Auth.tsx
+++ frontend/src/components/Auth.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+ 
 import {Link , useNavigate} from 'react-router-dom'
 import {useState, ChangeEvent} from 'react'
 import {SignUpInput} from '@rajratan/mindmint-common'
--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,5 +1,4 @@
-import React from 'react'
-
+ 
 export const Quote = () => {
     return <div className="bg-slate-200 h-screen flex justify-center flex-col">
         <div className="flex justify-center">
